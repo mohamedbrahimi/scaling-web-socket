44c1e8d46f81420f0d943f4c54a1f28b1a54d963
mohamedbrahimi <mohkriss3@gmail.com>
2020-06-27 22:37:08 +0100

    defined index namespaces.

---
+++ app/namespaces/index.mjs
@@ -0,0 +1,25 @@
+import { P2P_MESSAGES_WORKER_NAME_SPACE } from "../config/constants.mjs";
+import { produceP2pMessage } from "../helpers/queue/producers/index.mjs";
+const nameSpacesHandler = (io) => {
+
+    const usersP2pMessages = io.of(P2P_MESSAGES_WORKER_NAME_SPACE);
+
+    usersP2pMessages.on('connection', (socket) => {
+      console.log(`${P2P_MESSAGES_WORKER_NAME_SPACE}!`);
+      socket.on('room.join', (room) => {
+          socket.join(room);
+          console.log(`user join into: ${room}`);
+
+          socket.on('message', (message) => {
+              console.log(`message users/p2p/messages/ room/message: ${message}`);
+              produceP2pMessage(message);
+          });
+      });
+      socket.on('room.leave', (room) => { socket.leave(room, ()=>{}); console.log(`user leave from: ${room}`);});
+      socket.on('disconnect', (room) => {console.log(`user disconnected from: ${room}`);});
+    });
+
+  };
+
+export { nameSpacesHandler };
+
