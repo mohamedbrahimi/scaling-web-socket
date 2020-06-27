789a093d0572a8118ae29f45e3de31cdc25a6c27
mohamedbrahimi <mohkriss3@gmail.com>
2020-06-27 22:30:53 +0100

    constants file.

---
+++ app/config/constants.mjs
@@ -0,0 +1,3 @@
+export const P2P_MESSAGES_WORKER_NAME_SPACE = "/users/p2p/messages";
+export const P2P_MESSAGES_REDIS_TOPIC = "p2p-messages";
+export const P2P_MESSAGES_SOCKET_EVENT = "p2p.messages";
